022a0924dd690bff24007bd1350ac99a65f8bcbf
Alifsurya <arkandanalif@gmail.com>
2025-10-23 01:11:32 +0700

    Tugas 7 Laravel

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,12 +18,17 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api'
 
 
 Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+
+
 Route::middleware(['auth:api'])->group(function () {
 
     Route::apiResource('/genres', GenreController::class);
+
+    Route::apiResource('transactions', TransactionController::class)->only(['index', 'store', 'show']);
     
     Route::middleware(['role:admin'])->group(function () {
         Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('transactions', TransactionController::class)->only(['update', 'destroy']);
     });
 
 });
